5610bc01f0e06c4fb19dae12f607cd43138ab67e
Admin <Admin@DESKTOP-84DVP6E>
2020-11-17 22:20:15 +0530

    Added Account CRUD Operation

--- oiqgsWebAppProj/src/main/java/com/cg/oiqgsWebAppProj/service/AccountServiceImpl.java
+++ oiqgsWebAppProj/src/main/java/com/cg/oiqgsWebAppProj/service/AccountServiceImpl.java
@@ -5,20 +5,14 @@ import com.cg.oiqgsWebAppProj.dao.AccountRepo;
 import com.cg.oiqgsWebAppProj.dao.AccountRepoImpl;
 import com.cg.oiqgsWebAppProj.model.Accounts;
 public class AccountServiceImpl implements AccountService{
-   AccountRepo repo;
-	
+   AccountRepo repo;	
 	public AccountServiceImpl() throws SQLException {
 		repo=new AccountRepoImpl();
-	}
-	
-	
+	}	
 	@Override
-	public Accounts addAccount(Accounts account) throws SQLException {
-		
+	public Accounts addAccount(Accounts account) throws SQLException {		
 		return repo.addAccount(account);
-	}
-
-	
+	}	
 	@Override
 	public List<Accounts> getAllAccounts() throws SQLException {
 		// TODO Auto-generated method stub
